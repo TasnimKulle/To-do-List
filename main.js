984e640e382c9916981883a07673a4141c08b4b1
TasnimKulle <tasniim.tak@gmail.com>
2024-10-21 08:54:19 +0300

    update the localStorage

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function addTeskToDom(task) {
   li.className = `todoItem ${task.completed ? "completed" : ''}`
   li.dataset.id = task.id;
   li.innerHTML = `
-        <input type="checkbox" class="complete-checkbox">
+        <input type="checkbox" class="complete-checkbox" ${task.completed? 'checked' : ''}>
         <span class="task">${task.text}</span>
         <button class="edit-btn">Edit</button>
         <button class="delete-btn">Delete</button>
@@ -45,14 +45,32 @@ function addTeskToDom(task) {
     // console.log(li)
 }
 function attachEventListener(li,task){
+
   const delenBtn=li.querySelector('.delete-btn');
   const editBtn=li.querySelector('.edit-btn')
+  const checkbox=li.querySelector('.complete-checkbox')
   delenBtn.addEventListener('click',function(){
     handleDelete(task.id,li)
   })
   editBtn.addEventListener('click',function(){
     handleEdit(task.id,li)
+
   })
+  checkbox.addEventListener('change',function(){
+    toggleTaskCompletion(task.id,li,checkbox.checked)
+    console.log(task.text,checkbox.checked)
+  })
+}
+function toggleTaskCompletion(taskid,li,isCompleted){
+  const tasks=getTaskTolocalStorage();
+  const task=tasks.find(task=>task.id==taskid);
+  if(task){
+    task.completed=isCompleted;
+    localStorage.setItem('tasks',JSON.stringify(tasks))
+    li.classList.toggle('completed',isCompleted)
+  }
+
+
 }
 
 function handleEdit(taskid , li){
@@ -60,9 +78,19 @@ function handleEdit(taskid , li){
   // console.log(taskSpan.textContent)
   const newTaskText=prompt("EDIT TASK",taskSpan.textContent)
   if(newTaskText !== null && newTaskText.trim() !=="" ){
+     updateTask(taskid,newTaskText)
     taskSpan.textContent=newTaskText
   }
   
+  }
+  function updateTask(id,newTaskText){
+    const tasks=getTaskTolocalStorage()
+    const  task=tasks.find(task=>task.id ==  id)
+    if(task){
+      task.text=newTaskText
+      localStorage.setItem('tasks',JSON.stringify(tasks))
+    }
+
   }
 
 function handleDelete(id,li){
